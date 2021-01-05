5b84c6c2c6a2470a667873e8872071e3dd647c10
Sasha2125 <53381110+Sasha2125@users.noreply.github.com>
2021-01-05 18:39:43 +0200

    Add files via upload

---
+++ precache-manifest.a7114d7f155d32a9f3618ffb49ed3bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fdb63c276d5d745c8b85c9d33a3ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6f9ea0ceca2d98b7f6",
+    "url": "/static/css/main.ec05cfff.chunk.css"
+  },
+  {
+    "revision": "0077e9e05e94bba2482c",
+    "url": "/static/js/2.8309a81e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8309a81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6f9ea0ceca2d98b7f6",
+    "url": "/static/js/main.bfdf4438.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  }
+]);
